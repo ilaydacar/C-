505862bf33f97723b3a5421fa43fb249d3ab0517
ilaydacar <86203671+ilaydacar@users.noreply.github.com>
2021-06-23 21:26:22 +0300

    C# First Project

---
+++ C#.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlkProjem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ortanca bulalım.
+            int a, b, c;
+            Console.WriteLine("1.sayıyı giriniz.");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2.sayıyı giriniz.");
+            b= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("3.sayıyı giriniz.");
+            c= Convert.ToInt32(Console.ReadLine());
+
+            if((a<b && a>c)|| (a>b && a<c))
+            {
+                Console.WriteLine("{0}sayısı ortancadır", a);
+            }
+            else if ((b<a && b>c) || (b>a && b<c))
+            {
+                Console.WriteLine("{0}sayısı ortancadır", b);
+            }
+            else if ((c< a && c>b) || (c>a && c<b))
+            {
+                Console.WriteLine("{0}sayısı ortancadır", c);
+            }
+            else
+            {
+                Console.WriteLine("sayılar arasında eşitlik vardır.Ortanca yoktur.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
